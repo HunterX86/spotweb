1f9d12e48f27c8cdcdebc023d4a7b45df44f0f80
HunterX86 <tester4286@gmail.com>
2013-09-22 17:15:05 +0200

    added animation to navbar and added a menu

---
+++ templates/mobile/menu.inc.php
@@ -0,0 +1,11 @@
+<!-- panel content goes here -->
+<div data-role="panel" data-position="left" data-display="overlay" id="mypanel">
+    <ul data-role="listview" data-inset="false" data-divider-theme="a" data-theme="c">
+    	<li data-role="list-divider"><h1>Menu</h1></li>
+        <li data-icon="false"><a href="/spotweb/?page=edituserprefs&amp;userid=3">Voorkeuren wijzigen</a></li>
+		<li data-icon="false"><a href="?page=editsettings" target="_self">Instellingen</a></li>
+		<li data-icon="false"><a href="?page=render&amp;tplname=usermanagement">Gebruikers &amp; groepen beheer</a></li>
+		<li data-icon="false"><a href="" id="anchorLoginControl">Afmelden</a></li>
+    </ul>
+</div>
+<!-- ----------------------- -->
